624696188163f8f1f54063bc6d74064c5a1df213
azaho <anzahorodnii@gmail.com>
2025-10-23 14:54:35 -0400

    moving from mlni

---
+++ README.md
@@ -0,0 +1,75 @@
+# Neural Conversion Library for Standardized Data
+
+This repository provides tools for converting various iEEG neural data formats into a standardized `temporaldata` format. The goal is to facilitate data sharing, analysis, and interoperability across different research groups and studies.
+
+## Data Format
+
+```python
+session = Data(
+    # metadata
+    brainset = "STRING",
+    subject = "STRING",
+    session = "STRING",
+    
+    # In case the data includes iEEG. NTOE: EEG is also included here, as it is a generalization (the same data format).
+    ieeg = RegularTimeSeries(
+        data = seeg_data,  # Shape: (n_timepoints, n_electrodes). Voltage in uV
+        sampling_rate = 2048,  # Hz
+
+        domain_start = 0.0,  # Start at 0 seconds
+        domain = "auto"  # Let it infer from data length and sampling rate
+    ),
+    channels = ArrayDict(
+        id = np.array(["LAMY1"]), # Shape: (n_electrodes, )
+        
+        # Coordinates of the corresponding electrodes. Usually, these will be the MNI coordinates. 
+        # Note: in some datasets, there will be an exception (if MNI are unavailable or type of probe is different)
+        x = np.array([0.0]), # Shape: (n_electrodes, ). if unknown, can be np.nan
+        y = np.array([0.0]),
+        z = np.array([0.0]),
+        brain_area = np.array(["UNKNOWN"]),
+
+        type = np.array(["SEEG"]) # options: SEEG, ECOG, EEG, etc
+    ),
+    ieeg_artifacts = Interval(
+        start = np.array([0.0]), # shape: (n_artifacts, )
+        end = np.array([0.0]),
+        affected_channels = np.array([[1]], type=bool), # shape: (n_artifacts, n_electrodes)
+        description = np.array(["UNKNOWN"]),
+        timekeys = ['start', 'end'],  # Only time should be adjusted during operations
+    )
+
+    # In case the data includes any type of triggers. Note: These could be redundant with the other tags below.
+    triggers = IrregularTimeSeries(
+        timestamps = trigger_times,
+        type = np.array(["MOUSE_CLICK"]),
+        note = np.array([""]), # Optional note together with the trigger. Can be empty.
+        
+        timekeys = ['timestamps'],  # Only timestamps should be adjusted during operations
+    ),
+    
+    # In case the data includes stimulation. Note: frequency is not a parameter here! Use many electrical_stimulation events (as separate pulses) to denote the stimulation at a particular frequency.
+    electrical_stimulation = IrregularTimeSeries(
+        timestamps = stimulus_times,  # Shape (n_stim,). If multiple electrodes/electrode pairs at the same time, there will be multiple entries in the timestamp
+        waveform_type = np.array(["BIPHASIC"]),  # (n_stim,).
+        stimulation_site = np.array(["ELEC1-ELEC2"]),  # (n_stim,). Can be two electrode labels separated by a dash
+        amplitude = np.array([1.0]), # mA
+        pulsewidth = np.array([0.001]), # seconds
+        duration = np.array([0.014]), # seconds
+        timekeys = ['timestamps'],  # Only timestamps should be adjusted during operations
+    ),
+    
+    # In case the data includes images shown
+    images = IrregularTimeSeries(
+        timestamps = stimulus_times,  # When stimuli were shown. (n_stimuli,)
+        stimulus_ids = np.array(stimulus_images),  # Image identifiers
+        timekeys = ['timestamps'],  # Only timestamps should be adjusted during operations
+    ),
+    
+    # In case the data includes sound played
+    sounds = None, # same structure as images
+    continuous_sound = None, # unused for now, but may be RegularTimeSeries - the actual raw waveform of the sound
+    
+    domain=ieeg.domain
+)
+```
---
+++ datasets/base.py
@@ -0,0 +1,324 @@
+from __future__ import annotations  # allow compatibility for Python 3.9
+
+import datetime
+import logging
+import os
+import warnings
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import ClassVar
+
+import h5py
+import numpy as np
+import pandas as pd
+# from brainsets.descriptions import BrainsetDescription, DeviceDescription, SessionDescription, SubjectDescription
+# from brainsets.taxonomy import RecordingTech, Species
+from mne_bids import read_raw_bids
+from temporaldata import ArrayDict, Data, RegularTimeSeries
+
+logger = logging.getLogger(__name__)
+
+
+class SessionBase(ABC):
+    """
+    This class is an interface used to load the iEEG neural data for a given session. The dataset is assumed to be stored in the root_dir directory. This class must be used as a parent class for all session classes.
+    """
+
+    # NOTE: Every subclass must define these variables
+    dataset_identifier: ClassVar[str]  # Follow the brainsets convention for naming: firstAuthorLastName_lastAuthorLastName_firstWordOfPublicationTitle_publicationYear (all lowercase letters)
+    dataset_version: ClassVar[str]  # Version of the dataset.
+    name: ClassVar[str]
+    url: ClassVar[str | None]  # If empty, it will be assumed that the dataset is private
+    citation: ClassVar[str | None]  # In BibTex format. If empty, it will be assumed that the dataset is private. This can be multiple citations, separated by a newline.
+
+    def __init__(
+        self,
+        subject_identifier: str,
+        session_identifier: str,
+        root_dir: str | Path | None = None,
+        allow_corrupted: bool = False,
+    ):
+        # Check if the root_dir is set in the environment variables
+        if root_dir is None:
+            root_dir = self.find_root_dir()
+        if isinstance(root_dir, str):
+            root_dir = Path(root_dir)
+
+        self.root_dir = root_dir
+        self.subject_identifier = subject_identifier
+        self.session_identifier = session_identifier
+        self.allow_corrupted = allow_corrupted
+
+        self.data_dict = {
+            # Commented out while fixing the dependency issue with brainsets
+            # "brainset": BrainsetDescription(
+            #     id=self.dataset_identifier,
+            #     origin_version=self.dataset_version,
+            #     derived_version=self.dataset_version,
+            #     source=self.url,
+            #     description=self.name,
+            # ),
+            # "subject": SubjectDescription(
+            #     id=self.subject_identifier,
+            #     species=Species.HUMAN,
+            # ),
+            # "session": SessionDescription(
+            #     id=self.session_identifier,
+            #     recording_date=datetime.datetime.min,  # TODO: add recording date somehow from the data
+            # ),
+            # "device": DeviceDescription(
+            #     id="iEEG/EEG",
+            #     recording_tech=RecordingTech.ECOG_ARRAY_ECOGS,
+            # ),
+            "brainset": self.dataset_identifier,
+            "subject": self.subject_identifier,
+            "session": self.session_identifier,
+
+            "allow_corrupted": self.allow_corrupted,
+            "citation": self.citation,
+        }
+
+        # Discover subjects and ensure the subject identifier exists in the dataset
+        self.all_subjects = self.__class__.discover_subjects(self.root_dir)
+        assert self.subject_identifier in self.all_subjects, f"Subject {self.subject_identifier} not found in dataset. List of subjects: {self.all_subjects}"
+        self.subject_dir = self.root_dir / self.subject_identifier
+
+        # Discover sessions and ensure the session identifier exists in the dataset
+        self.all_sessions = self.__class__.discover_sessions(self.subject_identifier, root_dir=self.root_dir)
+        all_session_identifiers = [session["session_identifier"] for session in self.all_sessions]
+        assert self.session_identifier in all_session_identifiers, f"Session {self.session_identifier} not found in {self.all_sessions}"
+        self.session = self.all_sessions[all_session_identifiers.index(self.session_identifier)]
+
+    @classmethod
+    def find_root_dir(cls) -> str:
+        """
+        Find the root directory of the dataset in the environment variables.
+        """
+        try:
+            return os.environ["ROOT_DIR_" + cls.dataset_identifier.upper()]
+        except KeyError:
+            raise ValueError(f"When loading dataset {cls.dataset_identifier}, ROOT_DIR_{cls.dataset_identifier.upper()} not set in environment variables. Please either set the ROOT_DIR_{cls.dataset_identifier.upper()} environment variable or pass the root_dir argument to the constructor.") from None
+
+    @classmethod
+    @abstractmethod
+    def discover_subjects(cls, root_dir: str | Path | None = None) -> list:
+        """
+        Discover all subjects in the dataset.
+
+        Args:
+            root_dir (str | Path | None): The root directory of the dataset. If not provided, the root directory will be found in the environment variables.
+
+        Returns:
+            list: List of subject identifiers.
+        """
+        raise NotImplementedError("Not implemented")
+
+    @classmethod
+    @abstractmethod
+    def discover_sessions(cls, subject_identifier: str, root_dir: str | Path | None = None):
+        """
+        Discover available sessions for the subject subject_identifier in the root_dir. This is a static method that can be used to discover sessions for any subject in the dataset.
+
+        Args:
+            subject_identifier (str): The identifier of the subject to discover sessions for.
+            root_dir (str | Path | None): The root directory of the dataset. If not provided, the root directory will be found in the environment variables.
+
+        Returns:
+            list: List of dictionaries containing:
+                - session_identifier: the session identifier (e.g., "031411")
+                - events_file: path to the file containing the events
+                - ieeg_file: path to the file containing the iEEG data. Must be a BIDSPath object.
+                - ieeg_electrodes_file: path to the file containing the electrodes. This file will contain the coordinates of each electrode.
+                - ieeg_channels_file: path to the file containing the channels. This file will contain the labels of each channel and session-specific metadata (good vs bad channels, etc.)
+        """
+        raise NotImplementedError("Not implemented")
+
+    def get_data(self):
+        """
+        Get the data for the session in the temporaldata format.
+
+        Returns:
+            data: The data for the session in the format of a temporaldata.Data object.
+        """
+        return Data(**self.data_dict, domain="auto")
+
+    def save_data(self, save_root_dir: str | Path):
+        """
+        Save the data for the session in the temporaldata format.
+
+        Args:
+            save_root_dir (str | Path): The root directory to save the data to.
+
+        Returns:
+            path,data (tuple[str | Path, temporaldata.Data]): Tuple containing:
+                path: The path to the saved data.
+                data: The data for the session in the format of a temporaldata.Data object.
+        """
+        path = Path(save_root_dir) / self.dataset_identifier / self.subject_identifier / self.session_identifier
+        data = self.get_data()
+
+        if path.exists():
+            logger.info(f"Data for subject {self.subject_identifier} and session {self.session_identifier} already exists at {path}. Skipping.")
+            return path, data
+
+        path.mkdir(parents=True, exist_ok=True)
+
+        # Save to HDF5
+        with h5py.File(path / "data.h5", "w") as f:
+            data.to_hdf5(f)
+
+        logger.info(f"Saved data for subject {self.subject_identifier} and session {self.session_identifier} to {path}")
+        return path, data
+
+    @classmethod
+    def save_all_subjects_sessions(cls, root_dir: str | Path | None, save_root_dir: str | Path):
+        """Save all subjects and sessions to the specified directory.
+
+        Args:
+            root_dir (str | Path | None): Root directory of the dataset. If None, will be found in environment variables.
+            save_root_dir (str | Path): Root directory to save the processed data.
+        """
+        for subject_identifier in cls.discover_subjects(root_dir=root_dir):
+            for session in cls.discover_sessions(subject_identifier=subject_identifier, root_dir=root_dir):
+                session_identifier = session["session_identifier"]
+                if (Path(save_root_dir) / cls.dataset_identifier / subject_identifier / session_identifier / "data.h5").exists():
+                    print(f"Data for {cls.dataset_identifier}/{subject_identifier}/{session_identifier} already exists. Skipping.")
+                    continue
+
+                session = cls(
+                    subject_identifier=subject_identifier,
+                    session_identifier=session_identifier,
+                    root_dir=root_dir,
+                    allow_corrupted=False,
+                )
+                path, data = session.save_data(save_root_dir=save_root_dir)
+
+                session_length = data.ieeg.data.shape[0] / data.ieeg.sampling_rate
+                n_electrodes = data.ieeg.data.shape[1]
+                if "electrical_stimulation" in data.keys():
+                    n_stim_events = data.electrical_stimulation.timestamps.shape[0]
+                else:
+                    n_stim_events = 0
+                print(f"Saved data: {path}")
+                print(f"\t\tSession length: {session_length:.2f} seconds\t\t{n_electrodes} electrodes\t\t{n_stim_events} stimulation events")
+
+    def _load_ieeg_electrodes(self, electrodes_file: str | Path, channels_file: str | Path):
+        """
+        This is an optional function to implement (only if the session contains ieeg data). Load the electrodes from the electrodes file.
+
+        Args:
+            electrodes_file (str | Path): The path to the electrodes file.
+            channels_file (str | Path): The path to the channels file.
+
+        Returns:
+            electrodes (temporaldata.ArrayDict): The electrodes in the format of a temporaldata.ArrayDict. The labels are the channel names, the coordinates are the x, y, z coordinates of the electrodes, and the types are the types of the channels.
+        """
+        raise NotImplementedError("Not implemented")
+
+    def _load_ieeg_data(self, ieeg_file: str | Path, suppress_warnings: bool = True):
+        """
+        This is an optional function to implement (only if the session contains ieeg data). Load the iEEG data from the ieeg file.
+
+        Args:
+            ieeg_file (str | Path): The path to the iEEG file.
+            suppress_warnings (bool): Whether to suppress warnings when loading the iEEG data. Default is True.
+
+        Returns:
+            ieeg_data (temporaldata.RegularTimeSeries): The iEEG data in the format of a temporaldata.RegularTimeSeries. The data is a numpy array of shape (n_channels, n_samples), the sampling rate is the sampling rate of the data (int, in Hz), the domain start is 0, and the domain is automatically determined based on the data.
+        """
+        raise NotImplementedError("Not implemented")
+
+    def _load_electrical_stimulation(self):
+        """
+        This is an optional function to implement (only if the session contains electrical stimulation data). Load the electrical stimulation data from the electrical stimulation file.
+
+        Returns:
+            electrical_stimulation (temporaldata.IrregularTimeSeries): The electrical stimulation data in the format of a temporaldata.IrregularTimeSeries. The timestamps are the timestamps of the electrical stimulation, the stimulation_site are the sites of the electrical stimulation, the duration are the duration of the electrical stimulation, the waveform_type are the types of the electrical stimulation, the current are the currents of the electrical stimulation, the frequency are the frequencies of the electrical stimulation, the pulse_width are the pulse widths of the electrical stimulation, and the domain is automatically determined based on the data.
+        """
+        raise NotImplementedError("Not implemented")
+
+
+class BIDSSession(SessionBase):
+    """
+    This class is used to load the iEEG neural data for a given session from the OpenNeuro BIDS dataset file format as used in OpenNeuro. The dataset is assumed to be stored in the root_dir directory.
+    """
+
+    def __init__(
+        self,
+        subject_identifier: str,
+        session_identifier: str,
+        root_dir: str | Path | None = None,
+        allow_corrupted: bool = False,
+    ):
+        super().__init__(
+            subject_identifier,
+            session_identifier,
+            root_dir=root_dir,
+            allow_corrupted=allow_corrupted,
+        )
+
+        self.data_dict["channels"] = self._load_ieeg_electrodes(self.session["ieeg_electrodes_file"], self.session["ieeg_channels_file"])
+        self.data_dict["ieeg"] = self._load_ieeg_data(self.session["ieeg_file"])
+
+    @classmethod
+    def discover_subjects(cls, root_dir: str | Path | None = None) -> list:
+        if root_dir is None:
+            root_dir = cls.find_root_dir()
+        if isinstance(root_dir, str):
+            root_dir = Path(root_dir)
+
+        participants_file = root_dir / "participants.tsv"
+        if not participants_file.exists():
+            raise FileNotFoundError(f"participants.tsv not found in {root_dir} (looking for path: {participants_file})")
+
+        participants_df = pd.read_csv(participants_file, sep="\t")
+        assert "participant_id" in participants_df.columns, "participants.tsv found but no 'participant_id' column present"
+        return participants_df["participant_id"].to_list()
+
+    def _load_ieeg_electrodes(self, electrodes_file: str | Path, channels_file: str | Path) -> ArrayDict:
+        electrodes_df = pd.read_csv(electrodes_file, sep="\t")
+        channels_df = pd.read_csv(channels_file, sep="\t")
+
+        # Remove any rows that contain NaN values (usually meaning non-iEEG channels)
+        electrodes_df = electrodes_df.dropna()
+
+        # Filter channels to only include ECOG or SEEG types and good channels if not allowing corrupted data
+        if "type" in channels_df.columns:
+            channels_df = channels_df[channels_df["type"].str.upper().isin(["ECOG", "SEEG"])]
+        if ("status" in channels_df.columns) and (not self.allow_corrupted):
+            channels_df = channels_df[channels_df["status"].str.upper().isin(["GOOD"])]
+
+        # Merge electrode coordinates into channels dataframe
+        # For each channel, find the corresponding electrode and copy x, y, z coordinates
+        channels_df = channels_df[["name", "type"]].merge(electrodes_df[["name", "x", "y", "z"]], on="name", how="left")
+
+        electrodes = ArrayDict(
+            id=channels_df["name"].array.astype(str),
+            x=channels_df["x"].array.astype(float),
+            y=channels_df["y"].array.astype(float),
+            z=channels_df["z"].array.astype(float),
+            brain_area=np.array(["UNKNOWN"] * len(channels_df)),  # TODO: add brain area
+            type=channels_df["type"].array.astype(str),
+        )
+        return electrodes
+
+    def _load_ieeg_data(self, ieeg_file: str | Path, suppress_warnings: bool = True):
+        if suppress_warnings:
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", message="No BIDS -> MNE mapping found")
+                warnings.filterwarnings("ignore", message="Unable to map the following column")
+                warnings.filterwarnings("ignore", message="Not setting positions")
+                warnings.filterwarnings("ignore", message="DigMontage is only a subset of info.")
+                warnings.filterwarnings("ignore", category=RuntimeWarning, module="mne_bids")
+                raw = read_raw_bids(ieeg_file, verbose=False)
+        else:
+            raw = read_raw_bids(ieeg_file, verbose=True)
+
+        raw = raw.pick(self.data_dict["channels"].id.tolist())  # type: ignore[attr-defined]
+
+        return RegularTimeSeries(
+            data=raw.get_data().astype(np.float32).T * 1e6,  # shape should be (n_samples, n_channels), and convert to microvolts
+            sampling_rate=int(raw.info["sfreq"]),
+            domain_start=0.0,  # Start of the domain (in seconds)
+            domain="auto",  # Automatically determine the domain based on the data # type:ignore
+        )
